ff740d6dc795ba0e02a86e180a161a4d4d1661dd
Marco A.Bidoia <mabidoia@veltec.com.br>
2017-06-09 10:53:27 -0300

    Adiciona componente veiculo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VeiculoComponent } from "app/veiculo/veiculo.component";
 
 const routes: Routes = [
   {
     path: '',
     children: []
+  },
+  {
+    path: 'veiculo',
+    component: VeiculoComponent
   }
 ];
 
